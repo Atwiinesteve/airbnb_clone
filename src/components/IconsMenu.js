65c20e01ccde5df6f83422d312c4917a70604329
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2022-12-20 15:25:06 +0300

    Added icons menu component

--- src/components/IconsMenu.js
+++ src/components/IconsMenu.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+
+// importing styles
+import "../styles/IconsMenu.css";
 
 function IconsMenu() {
 
@@ -20,100 +23,254 @@ function IconsMenu() {
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/2539/2539238.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Amazing pools</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/1701/1701629.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Amazing views</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/8933/8933490.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Top of the World</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/98/98541.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Mansions</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
-								alt="caves"
+								src="https://cdn-icons-png.flaticon.com/512/3030/3030336.png"
+								alt="Private Rooms"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Private Rooms</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/5195/5195910.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>A-frames</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/768/768452.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>OMG</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/1616/1616302.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Lake</p>
 						</div>
 					</div>
 					<div className="icon--link">
 						<a href="/caves">
 							<img
-								src="https://cdn-icons-png.flaticon.com/512/1566/1566544.png"
+								src="https://cdn-icons-png.flaticon.com/512/3750/3750280.png"
 								alt="caves"
 							/>
 						</a>
 						<div className="icon--desc">
-							<p>Caves</p>
+							<p>Tiny homes</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/2975/2975876.png"
+								alt="Earth homes"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Earth homes</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/3078/3078085.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Camping</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/3765/3765735.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Luxe</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/3319/3319235.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Farms</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/1981/1981770.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Containers</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/2374/2374998.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Countryside</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/887/887322.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Bed & Breakfast</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/2550/2550287.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Trending</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/8301/8301196.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Design</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/4887/4887157.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>National Parks</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/606/606099.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Golfing</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/5795/5795208.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Arctic</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/2664/2664589.png"
+								alt="caves"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Beachfront</p>
+						</div>
+					</div>
+					<div className="icon--link">
+						<a href="/caves">
+							<img
+								src="https://cdn-icons-png.flaticon.com/512/6934/6934975.png"
+								alt="castles"
+							/>
+						</a>
+						<div className="icon--desc">
+							<p>Castles</p>
 						</div>
 					</div>
 				</div>
