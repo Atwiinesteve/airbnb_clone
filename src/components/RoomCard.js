46c775e84c247291178dc07b628d165b4a179e1c
Kiiza Atwiine Stephen <atwiinestephen123@gmail.com>
2022-12-20 16:00:10 +0300

    Added houses pictures

---
+++ src/components/RoomCard.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+// importing styles.
+import "../styles/RoomCard.css";
+
+function RoomCard({ image, title, rating, distance, date, price, time }) {
+
+  return (
+
+    <>
+      <div className='card'>
+        <div className='card--image'>
+            <img src={image} alt="house" />
+        </div>
+        <div className='card--title'>
+            <div className='title'>{title}</div>
+            <div className='rating'>{rating}</div>
+        </div>
+        <div className='card--distance'>
+            <p>{distance}</p>
+        </div>
+        <div className='card--date'>
+            <p>{date}</p>
+        </div>
+        <div className='card--price'>
+            <p className='amount'>{price}</p>
+            <p className='time'>{time}</p>
+        </div>
+      </div>
+    </>
+
+  );
+
+}
+
+export default RoomCard;
